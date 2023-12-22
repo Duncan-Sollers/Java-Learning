6cb9d070d7cb890b7f3f2ff88d3dbba7c1aab23b
BoondockTaints <wsollers@gmail.com>
2023-12-22 14:10:48 -0500

    impl comparator
    impl comparator

--- dev/duncan/programpractice/dec_12_22_2023/StringComparator.java
+++ dev/duncan/programpractice/dec_12_22_2023/StringComparator.java
@@ -2,25 +2,24 @@ package dev.duncan.programpractice.dec_12_22_2023;
 
 import java.util.Comparator;
 
-public class StringComparator implements Comparator {
+public class StringComparator implements Comparator<String> {
     /*
      * 0 means the same order
      * Less than < means lhs is bigger than rhs -1
      * Greater than > means rhs is bigger than lhs +1
      */
     @Override
-    public int compare(Object lhs, Object rhs) {
+    public int compare(String lhs, String rhs) {
         if (lhs.compareTo(rhs) == 0){
-
+          return 0;
         }
         if (lhs.compareTo(rhs) == 1){
-
+          return 1;
         }
         if (lhs.compareTo(rhs) == -1){
-
+          return -1;
         }
 
-        throw new UnsupportedOperationException("Unimplemented method 'compare'");
     }
     
 }
