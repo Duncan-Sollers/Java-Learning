a67e6acec2a940f6a1a0904767ce34e2da7d6a24
Duncan-Sollers <34609027+Duncan-Sollers@users.noreply.github.com>
2023-12-22 12:34:49 -0500

    Update ListTest.java

--- dev/duncan/programpractice/dec_12_22_2023/ListTest.java
+++ dev/duncan/programpractice/dec_12_22_2023/ListTest.java
@@ -1,6 +1,7 @@
 package dev.duncan.programpractice.dec_12_22_2023;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +10,7 @@ import org.junit.Test;
 
 
 public class ListTest {
+    public String str1 = "Element1";
     @Test
     public void testList(){
 
@@ -17,5 +19,24 @@ public class ListTest {
     public void testNewListIsEmpty(){
         assertEquals(true, new ArrayList<String>().isEmpty());
 
+    }
+    @Test
+    public void testNewListHasValue(){
+        List<String> arrayList = new ArrayList<String>();
+        arrayList.add(0,"Element1");
+        assertTrue(arrayList.contains("Element1"));
+
+    }
+    @Test
+    public void testTwoNewListsHaveSameValues(){
+        List<String> arrayListA = new ArrayList<String>();
+        List<String> arrayListB = new ArrayList<String>();
+        arrayListA.add(0,"Element1");
+        arrayListA.add(1,"Element2");
+        for(int i=0; i<arrayListA.size(); i++ ){
+            arrayListB.add(i, arrayListA.get(i));
+        }
+        assertTrue(arrayListB.contains("Element1"));
+        assertTrue(arrayListB.contains("Element2"));
     }
 }
