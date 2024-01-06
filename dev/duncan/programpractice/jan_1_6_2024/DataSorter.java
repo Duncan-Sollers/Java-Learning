fbc2f9ba5b03f0900487663fcfc8fef0ee9a84d9
Duncan-Sollers <34609027+Duncan-Sollers@users.noreply.github.com>
2024-01-06 17:35:03 -0500

    Created the PeopleInfo DTO

--- dev/duncan/programpractice/jan_1_6_2024/DataSorter.java
+++ dev/duncan/programpractice/jan_1_6_2024/DataSorter.java
@@ -3,6 +3,6 @@ package dev.duncan.programpractice.jan_1_6_2024;
 
 public class DataSorter {
     public static void main(String[] args) {
-        
+        System.out.println("test");
     }//
 }
---
+++ dev/duncan/programpractice/jan_1_6_2024/PeopleInfo.java
@@ -0,0 +1,45 @@
+package dev.duncan.programpractice.jan_1_6_2024;
+//@ PeopleInfo is the DTO for file
+public class PeopleInfo {
+
+
+    private String firstName;
+    private String lastName;
+    private Character sex;
+    private Integer age;
+    
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Character getSex() {
+        return this.sex;
+    }
+
+    public void setSex(Character sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    
+}
