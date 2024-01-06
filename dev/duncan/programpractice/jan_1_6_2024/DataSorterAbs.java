7239bc4e66e0398bf764a6ab780fd61c13da6a8d
Duncan-Sollers <34609027+Duncan-Sollers@users.noreply.github.com>
2024-01-06 17:09:08 -0500

    Update DataSorterAbs.java

---
+++ dev/duncan/programpractice/jan_1_6_2024/DataSorterAbs.java
@@ -0,0 +1,10 @@
+package dev.duncan.programpractice.jan_1_6_2024;
+
+public class DataSorterAbs {
+    //todo: read file
+        //todo: Convert data to object
+    //todo: request user input
+        //todo: add method(s) sort
+        //todo: add method print
+    //todo: return user request
+}
